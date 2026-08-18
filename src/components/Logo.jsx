1bcf767658cb84114be7eb3c9e6a39ada7f3b8b6
foregato <arthurforegato@hotmail.com>
2026-08-18 10:49:28 -0300

    cor da logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo({ className = '' }) {
   return (
     <img
-      src="/Logo/logo.png"
+      src="/Logo/logobranco.png"
       alt="Vila Office"
       className={`h-10 md:h-12 w-auto object-contain ${className}`}
     />
